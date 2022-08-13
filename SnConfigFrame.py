6fb7a825e270413b7d41d1a0317201c0cf220101
arianna-daq <arianna-daq@uci.edu>
2022-08-12 22:57:08 -0700

    Final edits for SetSstDacs() and dependent variables.

--- SnConfigFrame.py
+++ SnConfigFrame.py
@@ -1,8 +1,11 @@
 import numpy as np
 
+# Not Supposed to be Set should come from config file
 fDac = np.array([[21488, 25681], [21488, 25681], [21488, 25681],
                  [21488, 25681], [21488, 25681], [21488, 25681],
                  [21488, 25681], [21488, 25681]])
+
+
 
 def GetDac(ch, dc):
     return fDac[ch][dc]
