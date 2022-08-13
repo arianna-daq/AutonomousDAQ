6018ea964be00d55431dd5d29e681c30b9e793a5
arianna-daq <arianna-daq@uci.edu>
2022-08-12 21:02:19 -0700

    Trying method for handling I2C to DAC data transfer failure.

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ import SnConfigFrame as Conf
 bus = smbus.SMBus(1)
 
 def SetSstDACs():
-    """Sends the High and Low thresholds to the LTC2657 DAC Chip via I2C"""
+    """Sends the High and Low Thresholds to the LTC2657 DAC Chip via I2C"""
 
+    # Init Variables
     dadr, dv, dn = 0, 0, 0
     LSB, MSB = 0, 0
 
@@ -22,19 +23,30 @@ def SetSstDACs():
             for tries in range(C.MaxDacSetTries):
                 if dok == True:
                     break
-                
+
+
+                # Address for Each DAC Register
                 dn = (C.ChansPerLTC2657*C.NchanDacs) - (dc*C.ChansPerLTC2657) - C.ChansPerLTC2657 + (ch % C.ChansPerLTC2657)
-                
+
+                # Raise Error for Invalid Code for LTC2657 DAC Chip
                 if dn > 7:
                     print("TotDacs=%d, dc=%d, ch=%d, ChansPerLTC2657=%d, dn=%d\n" % (C.TotDacs, dc, ch, C.ChansPerLTC2657, dn))
                     raise("chan/dac combination too big for 3 bits!")
 
+                # Build Data Bitstream to Send to LTC2657 DAC Chip
                 dn |= (C.UpdateDacCmd << 4)
                 dv = Conf.GetDac(ch, dc)
                 MSB = (dv & 65280) >> 8
                 LSB = (dv & 255)
-                bus.write_i2c_block_data(dadr, dn, [int(MSB), int(LSB)])
-                dok = True
+
+                # Send Data Bitstream to DAC Chip via I2C
+                try:
+                    bus.write_i2c_block_data(dadr, dn, [int(MSB), int(LSB)])
+                    dok = True
+                except OSError:
+                    dok = False
+
+                print(ch, dc, tries, dok)
 
 if __name__=="__main__":
     SetSstDACs()
