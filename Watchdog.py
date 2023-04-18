5395b96851cdea91edf547e89d88c85c175300a2
arianna <110426290+arianna-daq@users.noreply.github.com>
2023-04-18 20:29:53 +0000

    update

--- Watchdog.py
+++ Watchdog.py
@@ -3,19 +3,16 @@ import os
 from SnConstants import *
 
 class Watchdog:
-    WDPeriod  = None
-    PrevReset = None
-
-    def Starter(self, WD_period):
-        Watchdog().WDPeriod = WD_period
+    def __init__(self, WD_period):
+        self.WDPeriod = WD_period
 
         temp = open("WD.dat", 'r+')
-        Watchdog().PrevReset = temp.read(1)
+        self.PrevReset = temp.read(1)
         temp.seek(0)
         temp.write("0")
         temp.close()
 
-        self.WD_timer = Timer(Watchdog().WDPeriod, self.RESET)
+        self.WD_timer = Timer(self.WDPeriod, self.RESET)
         self.WD_timer.start()        
 
     def RESET(self):
@@ -27,18 +24,18 @@ class Watchdog:
 
     def kick(self, *args):
         if args:
-            Watchdog().WDPeriod = args[0]
+            self.WDPeriod = args[0]
             self.WD_timer.cancel()
-            self.WD_timer = Timer(Watchdog().WDPeriod, self.RESET)
+            self.WD_timer = Timer(self.WDPeriod, self.RESET)
         else:
             self.WD_timer.cancel()
-            self.WD_timer = Timer(Watchdog().WDPeriod, self.RESET)
+            self.WD_timer = Timer(self.WDPeriod, self.RESET)
 
     def didWatchdogReset(self):
-        return int(Watchdog().PrevReset)
+        return int(self.PrevReset)
     
-    def values(self):
-        return Watchdog().WDPeriod
+    def __str__(self):
+        return self.WDPeriod
 
         
 if __name__=="__main__":
