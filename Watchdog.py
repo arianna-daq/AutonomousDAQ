dde414c1d113f69415f839e6fe91eb7cfc11a287
arianna <110426290+arianna-daq@users.noreply.github.com>
2023-04-18 03:56:38 +0000

    adding watchdog timer

--- Watchdog.py
+++ Watchdog.py
@@ -6,13 +6,25 @@ class Watchdog:
         self.period = WD_period
         self.WD_timer = Timer(self.period, self.RESET_RPi)
 
-    def RESET_RPi(self):
-        os.system('sudo shutdown -r now')
+        temp = open("WD.dat", 'r')
+        self.Prev_reset = temp.read(1)
+        temp.seek(0)
+        temp.write("0")
+        temp.close()
 
+    def RESET_RPi(self):
+        temp = open("WD.dat", 'w')
+        temp.write("1")
+        temp.close()
+
+        os.system('sudo shutdown -r now')
 
     def kick(self):
         self.WD_timer.cancel()
         self.WD_timer = Timer(self.period, self.RESET_RPi)
+
+    def didWatchdogReset(self):
+        return int(self.Prev_reset)
 
         
 
